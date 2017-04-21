8f472e7114ad278e0b78b6cc482e38e05f8d2122
Wenjun Qu <wenjun.qu@stonybrook.edu>
2017-04-21 18:34:46 -0400

    bug13

--- server.js
+++ server.js
@@ -1484,6 +1484,6 @@ app.get('/media/:id',function(req,res){
 })
 
 
-app.listen(8080, "127.0.0.1",function(){
+app.listen(8080, "172.31.64.118",function(){
 	console.log("Server listening on port " + 9000);
 })
